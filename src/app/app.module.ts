d240bfb058bc8d7f57abc5e5631d325e4f3ae77b
Aleksandr_Afanasev1 <Aleksandr_Afanasev1@epam.com>
2018-11-28 18:34:04 +0300

    Components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeadlineComponent } from './headline/headline.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseItemComponent } from './course-item/course-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeadlineComponent,
+    CourseListComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule,
