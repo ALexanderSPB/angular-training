527ae46cf1af51b157dfd0cd12c51de54dbe84fe
Aleksandr_Afanasev1 <Aleksandr_Afanasev1@epam.com>
2018-12-05 13:53:43 +0300

    Task2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CourseListComponent} from './course-list/course-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'courses-page', component: CourseListComponent },
+  { path: '', redirectTo: 'courses-page', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
